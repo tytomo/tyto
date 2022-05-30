6b533e191bd3493fb85efec6273f000f5b9f4d61
tytomo <106510248+tytomo@users.noreply.github.com>
2022-05-30 14:30:15 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "6cb4a758-45ca-4368-9a62-b1d0cf8d89ab",
                         "alterId": 0
                     }
                 ],
